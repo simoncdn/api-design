08fa5bb5810c6f8b0e72a59e53287c8659dfbb98
sdn <cdna.simon@gmail.com>
2024-04-17 21:58:39 +0200

    feat(project-config,server): typescript conversion

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./server.js";
-
-const port = process.env.PORT || 3400;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
-});
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.status(200);
-  res.json({ message: "Hello world!" });
+	res.status(200);
+	res.json({ message: "Hello world!" });
 });
 
 export default app;
